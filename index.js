45d2e7ffc31141362fc87b32b0aef08ff2f48c65
Krzysztof Pudłowski <djkormo@gmail.com>
2018-03-24 09:43:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
 
-    response.end("First node.js application in Azure 2018!");
+    response.end("First node.js application in Azure 2018.03.24!");
 });
 
 var port = process.env.PORT || 1337;
